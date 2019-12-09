34a4545af2cddd10be571a8508536df2a4690bb3
C_A_L_D <pengjunhw@vip.qq.com>
2019-12-09 19:28:17 +0800

    CRM项目框架搭建

---
+++ crmTWO/src/main/java/com/sc/Application.java
@@ -0,0 +1,15 @@
+package com.sc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.sc.mapper")  
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
